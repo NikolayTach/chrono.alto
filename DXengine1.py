42e4631df16dce4a2f7f484c2d03d3986fc87f79
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2019-02-10 23:43:11 +0300

    Add files via upload

---
+++ DXengine1.py
@@ -0,0 +1,16 @@
+import KSengine1
+
+   print = "Center":
+   value = c-1/s-2 recieve chrono.file(1) 
+          is making path = 0
+   for </0.121>, </0.115>DXengine1, </1.141>
+   print = "Read" 
+   print = "Unread"
+      in value recieve chrono.file(1)
+         vertical
+        break 
+          log = (1)KSengineDD.1
+      as mode 3# sand
+        or Dxengine1 cycle path chrono.file(1), make from Dxengine1 Def revision  
+   value return Dxengine1 chrono.file(1)
+ from usr/bin 
