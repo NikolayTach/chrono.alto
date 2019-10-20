d785d306d28b5edf187961e9d032a1ecbf91e931
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2019-10-20 07:42:07 -0700

    Create AmCEngine.py

---
+++ AmCEngine.py
@@ -0,0 +1,31 @@
+AmC = placed (read)
+    As 
+      Durable location 
+        message etc/dsn/file01
+        message etc/dsn/file01
+        message etc/dsn/file01
+        message etc/dsn/file01
+        message etc/dsn/file01
+        message etc/dsn/file01
+ /
+   if 
+      value of 3:1011
+     than 
+     3/1011
+     3/1001
+     3/1000
+     3/1011
+     3/1001
+     3/1000
+     3/1011
+     3/1001
+     3/1000
+            Typed Num Axial 
+        As
+         Served 
+     in
+        dsn/file01 
+        dsn/file01
+        dsn/file01
+ /
+        
