9b3735a43a424c9bcbfdb4ec757b34b6737a5053
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2020-04-25 01:18:52 -0700

    Create ex[1].py

---
+++ ex[1].py
@@ -0,0 +1,13 @@
+import @DXenngine.py durable location etc/dsn/file01
+import @AmCengine.py durable location etc/dsn/file01
+impory @TMProuterengine durable location etc/dsn/file01 
+
+use in for frames '/n'
+
+read durable patches through the stats.doc 
+
+create graphicx.py model 
+    in forms of scale ["%D"], ["%H"], ["%S"] 
+      frame int 
+       languadge 
+      \. Please select durable mode from graph model \.
