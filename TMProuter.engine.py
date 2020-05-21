ae963c58e620f4867e69c7cc3c21ff62a49827c7
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2020-05-21 11:17:24 -0700

    Update TMProuter.engine.py

--- TMProuter.engine.py
+++ TMProuter.engine.py
@@ -5,13 +5,13 @@ while
     or 
     temperature = down to -30  ° C 
 AmC do 
-   single pair joined by AmCEngine.py
+   single pair joined by AmCEngine
    first = showing #error122
            showing #error122
        value of 3:1011
        value of 3:1011
       message = 'Maping site of duration file is center in located chnono.alto'
-   Reads ionic preassure in per%cent 
+   Reads ionic preassure in percent 
   import 
     from 
   KSengine 
