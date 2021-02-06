061440c7cffed2273540277c5ecb98eacfe15207
NikolayTach <NikolayTach@users.noreply.github.com>
2021-02-07 01:55:55 +0300

    new
    C core

---
+++ barryiV.c
@@ -0,0 +1,20 @@
+include <sanuns.h> 
+incluse <barrends.h> 
+include <sorrows.h> 
+vasel at num reform 
+"etc/action/aggc" 
+abuse shape to (NULL ) 
+slow set int edd prone sack (FULL) set
+forhead of one load_set0/sdd.h forhead of one load_set0/muu.h forhead of one load_set0/css.h 
+
+lock dim follow as prone knaps on plat form_suit 
+ Date: "01/02/21" 
+   default settings all set null map proved amed lisp "load_set0" int madder inventory 
+   ":>" 
+   vasel at num = e reform "etc/action/aggc" abuse shape to (NULL) as nots int edd prone sack (FULL) set
+forhead of one load_set1/sdd.h forhead of one load_set1/muu.h forhead of one load_set1/css.h 
+lock dim follows as prone knaps on platform_suit 
+set 
+Date: "01/02/21" default settings all set null nap provcd aned lisp "load_set1" int nadder inventory
+ "<:" 
+   vasel at num = e reform 
