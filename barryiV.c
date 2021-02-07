9c68fc97ecd480f4fa8ab8bd7af33c25db79a20c
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-02-07 10:12:19 +0300

    Update barryiV.c

--- barryiV.c
+++ barryiV.c
@@ -1,21 +1,30 @@
 include <sanuns.h> 
-incluse <barrends.h> 
+include <barrends.h> 
 include <sorrows.h> 
 
-vasel at num reform 
+vasel at num = 0repo 
 "etc/action/aggc" 
 abuse shape to (NULL ) 
 slow set int edd prone sack (FULL) set
-forhead of one load_set0/sdd.h forhead of one load_set0/muu.h forhead of one load_set0/css.h 
-
-lock dim follow as prone knaps on plat form_suit 
+forhead of one "load_set0/sdd.h" forhead of one 
+"load_set0/muu.h" -a forhead of one 
+"load_set0/css.h" 
+lock dim follow as prone knaps on platform_suit 
+ set
  Date: "01/02/21" 
    default settings all set null map proved amed lisp "load_set0" int madder inventory 
    ":>" 
-   vasel at num = e reform "etc/action/aggc" abuse shape to (NULL) as nots int edd prone sack (FULL) set
-forhead of one load_set1/sdd.h forhead of one load_set1/muu.h forhead of one load_set1/css.h 
-lock dim follows as prone knaps on platform_suit 
+  
+   vasel at num = 1repo 
+   "etc/action/aggc"
+   abuse shape to (NULL) 
+  fast set int edd prone sack (FULL) set
+forhead of one "load_set1/sdd.h" forhead of one 
+"load_set1/muu.h" -a forhead of one 
+"load_set1/css.h" 
+   lock dim follows as prone knaps on platform_suit 
 set 
-Date: "01/02/21" default settings all set null nap provcd aned lisp "load_set1" int nadder inventory
+Date: "01/02/21" 
+ default settings all set null nap proved amed lisp "load_set1" int madder inventory
  "<:" 
    
