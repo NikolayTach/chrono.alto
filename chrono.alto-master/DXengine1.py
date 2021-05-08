336138474f2b1c5bd11fa07eecd8e63163a2e243
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-05-07 20:25:06 -0700

    Update DXengine1.py

--- chrono.alto-master/DXengine1.py
+++ chrono.alto-master/DXengine1.py
@@ -2,7 +2,7 @@ import KSengine1
 import AmCengine 
 import TMProuterengine 
 
-   print = "Center":
+   def main():
    value = c-1/s-2 recieve chrono.file(1) 
           is making path = 0
    for </0.121>, </0.115>DXengine1, </1.141>
@@ -11,32 +11,29 @@ import TMProuterengine
 in value recieve chrono.file(1)
          vertical
 break 
-          log = (1)KSengineDD.1
+        if  log = (1)KSengineDD.1
       as mode 3# sand
         or Dxengine1 cycle path chrono.file(1), make from Dxengine1 Def revision  
    value return Dxengine1 chrono.file(1)
  from usr/bin 
      get 'cases'
       square
-   All 4 cases
-            %444 = DDD
-            %555 = CCC
-            %666 = BBB
-            %777 = AAA
+     class Comment:
+   All 4 classes
+        #   %444 : DDD
+        #   %555 : CCC
+        #   %666 : BBB
+        #   %777 : AAA
             
   log = (2)KSengineDD.1
             as mode 3# sand
         or Dxengine1 cycle path chrono.file(1), make from Dxengine1 Def revision  
    value return Dxengine1 chrono.file(1)
- from usr/bin 
-     get 'case'
+ from etc/dsn/.
+     @stats.doc(PK$joPetc=первый режим, PK222$joP2etc=сход роуминга,PK^C^D^T$joP^Hetc=компиляр пакетов via chrono.alto)
        square
-       All 4 cases
-            %444 = DDD
-            %555 = CCC
-            %666 = BBB
-            %777 = AAA
-     
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
->>>><<<<<<>>><<<<<>><<<>><><<<<<<<<<<<<><>><<<><<<><<<<>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<><<<>
-
+       All 4 classes
+        #   %444 : DDD
+        #   %555 : CCC
+        #   %666 : BBB
+        #   %777 : AAA
