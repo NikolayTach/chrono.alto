11a8c54eff3dcdb5223adfbcb1198080c7c247f0
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-10-16 03:48:50 -0700

    Update alignment_test.rs

--- chrono.alto-master/alignment_test.rs
+++ chrono.alto-master/alignment_test.rs
@@ -1,7 +1,7 @@
 use pretty_assertions::assert_eq;
 use comfy_table::*;
 fn main(private_key) 
-//Nix_OS pkgs reformed in the inventory that has been linked to the rendering data doe fork.//
+/// Nix_OS pkgs reformed in the inventory that has been linked to the rendering data doe fork.
 #[test]
 /// Cell alignment can be specified on Columns and Cells
 /// Alignment settings on Cells overwrite the settings of Columns
