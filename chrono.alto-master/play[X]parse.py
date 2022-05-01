7748eb5f5ccbc074430dc9120a5da5969950dd81
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-05-02 00:02:54 +0300

    Add files via upload

---
+++ chrono.alto-master/play[X]parse.py
@@ -0,0 +1,33 @@
+import TMPengine
+import KSengine 
+import DXengine 
+
+count:()
+import itertools
+for i itertools():
+time.sleep(5) 
+print print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK222$joP2etc/dsn/PK2!joP2etc/dsn')
+print('PK2222$joP22etc/dsn/PK2222!joP22&etc/dsn')
+print('PK222$joP2etc/dsn/PK2!joP2etc/dsn')
+print('PK2222$joP22etc/dsn/PK2222!joP22&etc/dsn')
+print('PK222$joP2etc/dsn/PK2!joP2etc/dsn')
+print('PK2222$joP22etc/dsn/PK2222!joP22&etc/dsn')
+print('PK222$joP2etc/dsn/PK2!joP2etc/dsn')
+print('PK2222$joP22etc/dsn/PK2222!joP22&etc/dsn')
+print('PK222$joP2etc/dsn/PK2!joP2etc/dsn')
+print('PK2222$joP22etc/dsn/PK2222!joP22&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+print('PK^C^D^T$joP^Hetc/dsn/PK^C^D^T!joP^Oetc/dsn')
+print('PK^A^B^T^T$joP^H^Petc/dsn/PK^A^B^T^T!joP^O^P&etc/dsn')
+f= open ('chrono.altoLOG.txt','w')
