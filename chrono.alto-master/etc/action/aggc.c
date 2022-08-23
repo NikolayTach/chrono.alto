f359b80913cb17ebc17784f98dd48e17920bb093
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-08-22 18:55:12 -0700

    Create aggc.c

---
+++ chrono.alto-master/etc/action/aggc.c
@@ -0,0 +1,40 @@
+#include "studio.h"
+#include <sanuns.h> 
+#include <kool.h> 
+#include <sorrows.h> 
+
+
+#define "host.h"
+#define "vaked.ch"
+
+      {
+   def_function = ref:01 
+      to scale reader focus 
+      do
+   def_funtion  = ref:02
+      to scale reader focus 
+  };
+    {    
+        ref : "etc/dsn/stats.doc"
+            scale[0];
+                      for  null "PK_@222/dsn/etc/"
+         hold 
+                        bash
+ ref : "chrono.alto/chrono.alto-master/load_set0/kool.h"
+ host compile for 
+              quary  
+              def_funtion to scale reader front do
+    for 
+                make focus 
+                          tree 
+                         1 3 TMP
+                         2 7 AmC
+                      3   13 DX
+                      4 127 KS
+                      5 3423  TMP
+                    6 1235  AmC
+            linked:ref:01
+            linked:ref:02
+            linked:ref:01
+            linked:ref:02
+      };
