24c3a50119274516dc896397676354aad1edd3f9
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2024-02-11 00:01:39 -0500

    Update ca.py

--- chrono.alto-master/ca.py
+++ chrono.alto-master/ca.py
@@ -4,7 +4,7 @@ import DXengine
 
 for i in range(10):
     print (i)
-print('PK$joPetc — первый режим') 
-print('PK222$joP2etc — сход роуминга')
-print('PK^C^D^T$joP^Hetc — компиляр пакетов via chrono.alto')
+print('PK$joPetc - первый режим') 
+print('PK222$joP2etc - сход роуминга')
+print('PK^C^D^T$joP^Hetc - компиляр пакетов via chrono.alto')
 print('Все!')
